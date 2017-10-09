d08df372095327cd91d9a0efad316c4f86333187
Ariel Rivero <ariel_2005_06@hotmail.com>
2017-10-08 22:15:44 -0300

    corrijo el modal

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
 	<meta charset="utf-8">
 	<title>Logística LogIkw</title>
 
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<script src="js/jquery-3.2.1.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </head>
 <body>
 
@@ -21,13 +22,11 @@
       <li><a href="#">Page 2</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user" data-toggle="modal" data-target="#registro"></span> Registrarse</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in" data-toggle="modal" data-target="#login"></span> Login</a></li>
+      <li><a href="#"  data-toggle="modal" data-target="#registro"><span class="glyphicon glyphicon-user"></span> Registrarse</a></li>
+      <li><a href="#" data-toggle="modal" data-target="#login"><span class="glyphicon glyphicon-log-in" ></span> Login</a></li>
     </ul>
   </div>
 </nav>
-
-
 
 
 
@@ -43,13 +42,37 @@
 	        </button>
 	      </div>
 	      <div class="modal-body">
-	        <input type="text" name="nombre" placeholder="Juan Roman">
-	        <input type="text" name="apellido" placeholder="Fernandez">
-	        <input type="text" name="DNI" placeholder="12345678">
-	        <input type="text" name="Razon Social" placeholder="Sociedad Log S.A">
-	        <input type="text" name="Usuario" placeholder="socsa1234">
-	        <input type="text" name="Password1" placeholder="**********">
+	        <input type="text" name="nombre" placeholder="Juan Roman"><br>
+	        <input type="text" name="apellido" placeholder="Fernandez"><br>
+	        <input type="text" name="DNI" placeholder="12345678"><br>
+	        <input type="text" name="Razon Social" placeholder="Sociedad Log S.A"><br>
+	        <input type="text" name="Usuario" placeholder="socsa1234"><br>
+	        <input type="text" name="Password1" placeholder="**********"><br>
 	        <input type="text" name="Password2" placeholder="**********">
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+	        <button type="button" class="btn btn-primary">Save changes</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+	</form>
+
+		<!-- Modal -->
+	<form action="/Login">
+	<div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	  <div class="modal-dialog" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+	      <div class="modal-body">
+	        <input type="text" name="Usuario" placeholder="socsa1234"><br>
+	        <input type="text" name="Password1" placeholder="**********">
 	      </div>
 	      <div class="modal-footer">
 	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
