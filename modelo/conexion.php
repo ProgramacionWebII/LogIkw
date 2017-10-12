920e15f648a3bae8ac7bd1e7fd03214dfccd0965
Ariel Rivero <ariel_2005_06@hotmail.com>
2017-10-12 11:17:24 -0300

    Comento "Conexion" y "login"

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -2,25 +2,30 @@
 
   class Conexion{
 
+    /*Funcion que se invoca al principio para conectar a la BDD*/
     public static function conectar(){
       $con = mysqli_connect("localhost","root","","logikw");
       return $con;
     }
 
+    /*Esta funcion se invoca para mandar consultas de las que no necesitemos que nos de un retorno, por ejemplo un INSERT, DELETE, UPDATE*/
     public static function setQuery($sql){
       mysqli_query($con, $sql);
     }
 
+    /*Esta funcion se usa cuando quiero mandar un SELECT EJ:
+    SELECT * FROM administrador WHERE id_administrador = 1
+    */
     public static function getQuery($sql){
       $con = mysqli_connect("localhost","root","","logikw");
       $datos = mysqli_query($con, $sql);
-    $i = 0;
-    while($rs = mysqli_fetch_assoc($datos)){
-      echo $rs[$i];
-      $i++;
-  }
+      $i = 0;
+      while($rs = mysqli_fetch_assoc($datos)){
+        echo $rs[$i];
+        $i++;
+      }
 
       return $datos;
-  }
+    }
 }
 ?>
--- modelo/login.php
+++ modelo/login.php
@@ -1,13 +1,22 @@
 <?php
 include 'conexion.php';
 	session_start();
+
+	/*Conecto a la BDD primeramente para comenzar a operar por medio de ella*/
 	$con = Conexion::conectar();
+
+	/*Obengo el usuario y pass enviado por POST desde el formulario "login" */
 	$usuario = $_POST["usuario"];
 	$password = $_POST["password"];
+
+
 	$rol;
 
+	/*Preparo la consulta que voy a querer envía a la BDD por medio de los métodos de la clase "Conexion"*/
 	$query = "SELECT * FROM usuario WHERE usuario = '$usuario' AND pass = '$password'";
 
+	/*Envío la consulta al método "getQuery" para que me devuelva el resultado de ese SELECT*/
+	/*Por alguna razón devuelve un resultado vacío (a solucionar)*/
 	$resultado = Conexion::getQuery($query);
 	session_destroy();
 ?>
