bf879512c78a38c3b2a5b9959e8bedec0375e1a1
Ariel Rivero <ariel_2005_06@hotmail.com>
2017-10-16 21:34:50 -0300

    Agregué cierre de conexión a la DB cuando se deslogea el usuario

--- modelo/logout.php
+++ modelo/logout.php
@@ -2,6 +2,9 @@
 	session_start();
 	if($_SESSION['administrador'] == true || $_SESSION['chofer'] == true || 
 	$_SESSION['cliente'] == true || $_SESSION['mecanico'] == true || $_SESSION['empresa'] == true){
+		include "conexion.php";
+
+		Conexion::cerrar();
 		session_destroy();
 		header("Location: ../index.php");
 	}
